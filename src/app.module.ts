703a9f8bb3204a1bc8a1d96700d266e48ddea416
narek4241 <narekghazaryan02@gmail.com>
2020-12-04 00:28:57 +0400

    added products module, mongoDB, schema

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,17 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { ProductsController } from './products/products.controller';
-import { ProductsService } from './products/products.service';
+import { ProductsModule } from './products/products.module';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [],
-  controllers: [AppController, ProductsController],
-  providers: [AppService, ProductsService],
+  imports: [
+    ProductsModule,
+    MongooseModule.forRoot(
+      'mongodb+srv://nestUser:cztJWZFNR2Ubu7m@nest-rest-api.dqykk.mongodb.net/products?retryWrites=true&w=majority',
+    ),
+  ],
+  controllers: [AppController],
+  providers: [AppService],
 })
 export class AppModule {}
---
+++ src/products/products.module.ts
@@ -0,0 +1,19 @@
+import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ProductsController } from './products.controller';
+import { ProductsService } from './products.service';
+import { Product, ProductSchema } from './schemas/products.schema';
+
+@Module({
+  controllers: [ProductsController],
+  providers: [ProductsService],
+  imports: [
+    MongooseModule.forFeature([
+      {
+        name: Product.name,
+        schema: ProductSchema,
+      },
+    ]),
+  ],
+})
+export class ProductsModule {}
